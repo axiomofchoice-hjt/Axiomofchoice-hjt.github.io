10abe3ed37e7134273f120160f60878dc214ee24
Axiomofchoice <1939696303@qq.com>
2022-05-03 14:56:34 +0800

    'deploy'

---
+++ assets/js/40.25fd98be.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{582:function(n,w,o){}}]);
--- blockchallenge/static/Stages/Stage5.js
+++ blockchallenge/static/Stages/Stage5.js
@@ -5,7 +5,7 @@ stageList.push(function (id) {
     message: `第${toChinese(id)}关 找零游戏 请点击数字 注：灰色方块的数字仅表示位置`,
     task: [
       [0, 1, equal],
-      [0, 9, lessEqual],
+      [0, 10, lessEqual],
     ],
   });
   stage.init = function () {
